d3a4937006169cb9e97daaf2e859e646ab6646b2
Walter Nadalin <walternadalin4@gmail.com>
2022-07-14 14:39:07 +0200

    Inverted axes

--- Code/simulation/plotting.py
+++ Code/simulation/plotting.py
@@ -3,23 +3,32 @@ from numpy import linspace
 from matplotlib.pyplot import xlabel, ylabel, savefig, close, title
 
 def heat_map(name: str, x_range: tuple, y_range: tuple, values: list, *, x_n: int = 10, y_n: int = 10) -> None:
-  image = heatmap(values, cmap = 'jet', xticklabels = False)
+  '''
+  Prints a beautiful heatmap given a matrix of values through space and time.
 
-  x_ticks = linspace(0, values.shape[1], x_n)
-  x_labels = linspace(*x_range, x_n)
+  Parameters
+  ---------
+  ...
+  '''
+  image = heatmap(values.transpose(), cmap = 'jet', xticklabels = False)
+
+  x_ticks = linspace(0, values.shape[0], y_n)
+  x_labels = linspace(*y_range, y_n)
   x_labels = x_labels.astype(int)
+
+  y_ticks = linspace(0, values.shape[1], x_n)
+  y_labels = linspace(*x_range, x_n)
+  y_labels = y_labels.astype(int)
 
   image.set_xticks(x_ticks)
   image.set_xticklabels(x_labels)
 
-  y_ticks = linspace(0, values.shape[0], y_n)
-  y_labels = linspace(*y_range, y_n)
-  y_labels = y_labels.astype(int)
   image.set_yticks(y_ticks)
   image.set_yticklabels(y_labels)
 
-  xlabel('Space')
-  ylabel('Time')
+  xlabel('Time')
+  ylabel('Space')
   title('Variation of the density through space and time')
+
   savefig(f'./results/{name}.png')
   close()
